326346dc2300c5f7288ca9f835ae34b6d50612b4
s17v3r <o-gybanov@mail.ru>
2024-05-21 20:56:49 +0500

    fix pep8 warnings
    лишние отступы и слишком длинные строки

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
-from transformers import TrOCRProcessor, VisionEncoderDecoderModel, AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import (
+    TrOCRProcessor,
+    VisionEncoderDecoderModel,
+    AutoTokenizer,
+    AutoModelForSeq2SeqLM,
+)
 from PIL import Image
 import streamlit as st
 
-processor = TrOCRProcessor.from_pretrained('raxtemur/trocr-base-ru', from_pt=True)
-text_recognition_model = VisionEncoderDecoderModel.from_pretrained('raxtemur/trocr-base-ru')
+ocr_model_id = "raxtemur/trocr-base-ru"
+processor = TrOCRProcessor.from_pretrained(ocr_model_id, from_pt=True)
+ocr_model = VisionEncoderDecoderModel.from_pretrained(ocr_model_id)
 
-tokenizer = AutoTokenizer.from_pretrained("ai-forever/sage-fredt5-distilled-95m")
-typos_model = AutoModelForSeq2SeqLM.from_pretrained("ai-forever/sage-fredt5-distilled-95m")
+spell_check_model_id = "ai-forever/sage-fredt5-distilled-95m"
+tokenizer = AutoTokenizer.from_pretrained(spell_check_model_id)
+spell_check_model = AutoModelForSeq2SeqLM.from_pretrained(spell_check_model_id)
 
-img = st.file_uploader(label='Загрузите изображение')
+img = st.file_uploader(label="Загрузите изображение")
 if img is not None:
     image = Image.open(img)
     pixel_values = processor(images=image, return_tensors="pt").pixel_values
-    generated_ids = text_recognition_model.generate(pixel_values)
-    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-    st.text(generated_text)
+    ids = ocr_model.generate(pixel_values)
+    text = processor.batch_decode(ids, skip_special_tokens=True)[0]
+    st.text(text)
 
-    inputs = tokenizer(generated_text, max_length=None, padding="longest", truncation=False, return_tensors="pt")
-    outputs = typos_model.generate(**inputs.to(typos_model.device), max_length = inputs["input_ids"].size(1) * 1.5)
+    inputs = tokenizer(
+        text,
+        max_length=None,
+        padding="longest",
+        truncation=False,
+        return_tensors="pt",
+    )
+
+    outputs = spell_check_model.generate(
+        **inputs.to(spell_check_model.device),
+        max_length=inputs["input_ids"].size(1) * 1.5
+    )
+
     result = tokenizer.batch_decode(outputs, skip_special_tokens=True)
     st.text(result)
