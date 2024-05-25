4000d7ed6fafb9f2fce9e8df3fd5396d13389df1
s17v3r <o-gybanov@mail.ru>
2024-05-25 21:11:32 +0500

    перенес обработку изображения в модуль models

--- main.py
+++ main.py
@@ -3,29 +3,9 @@ import streamlit as st
 import models
 
 
-processor, ocr_model = models.load_ocr_models()
-tokenizer, spell_check_model = models.load_spell_check_models()
-
 img = st.file_uploader(label="Загрузите изображение")
 if img is not None:
     image = Image.open(img)
-    pixel_values = processor(images=image, return_tensors="pt").pixel_values
-    ids = ocr_model.generate(pixel_values)
-    text = processor.batch_decode(ids, skip_special_tokens=True)[0]
-    st.text(text)
-
-    inputs = tokenizer(
-        text,
-        max_length=None,
-        padding="longest",
-        truncation=False,
-        return_tensors="pt",
-    )
-
-    outputs = spell_check_model.generate(
-        **inputs.to(spell_check_model.device),
-        max_length=inputs["input_ids"].size(1) * 1.5
-    )
-
-    result = tokenizer.batch_decode(outputs, skip_special_tokens=True)
-    st.text(result)
+    recognized_text, correct_text = models.process_image(image)
+    st.text(recognized_text)
+    st.text(correct_text)
--- models.py
+++ models.py
@@ -22,3 +22,37 @@ def load_spell_check_models():
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     spell_check_model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
     return tokenizer, spell_check_model
+
+
+def process_image(image):
+    recognized_text = recognize_text_from_image(image)
+    correct_text = check_spelling_in_text(recognized_text)
+    return recognized_text, correct_text
+
+
+def recognize_text_from_image(image):
+    processor, ocr_model = load_ocr_models()
+    pixel_values = processor(images=image, return_tensors="pt").pixel_values
+    ids = ocr_model.generate(pixel_values)
+    text = processor.batch_decode(ids, skip_special_tokens=True)[0]
+    return text
+
+
+def check_spelling_in_text(text):
+    tokenizer, spell_check_model = load_spell_check_models()
+
+    inputs = tokenizer(
+        text,
+        max_length=None,
+        padding="longest",
+        truncation=False,
+        return_tensors="pt",
+    )
+
+    outputs = spell_check_model.generate(
+        **inputs.to(spell_check_model.device),
+        max_length=inputs["input_ids"].size(1) * 1.5
+    )
+
+    correct_text = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+    return correct_text
