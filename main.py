a49c99233543c5b4edfb12c44b82a956b67f6f8a
s17v3r <o-gybanov@mail.ru>
2024-05-25 22:17:06 +0500

    добавил вывод загруженного изображения и подписи

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import streamlit as st
 import models
 
 
-img = st.file_uploader(label="Загрузите изображение")
+img = st.file_uploader(label="Загрузите изображение", accept_multiple_files=False)
 if img is not None:
     image = Image.open(img)
+    st.text("Загруженное изображение:")
+    st.image(image)
+
     recognized_text, correct_text = models.process_image(image)
-    st.text(recognized_text)
-    st.text(correct_text)
+    st.text("Распознанный текст: " + recognized_text)
+    st.text("Правильный текст: " + correct_text)
+
